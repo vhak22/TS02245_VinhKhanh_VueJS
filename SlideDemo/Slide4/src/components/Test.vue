4a16d2343ac1da5ca2c366e6c9ce8b97e3d3d226
vinhkhanh <khanhnguyeen2208@gmail.com>
2026-01-15 22:37:35 +0700

    create demo Slide4(vue project)

---
+++ SlideDemo/Slide4/src/components/Test.vue
@@ -0,0 +1,27 @@
+<script>
+export default {
+  data() {
+    return {
+      count: 0
+    }
+  }, // <--- Nhớ dấu phẩy ở đây để ngăn cách data và methods
+  methods: {
+    reset() {
+      this.count = 0
+    },
+    increment() {
+      this.count++
+    }
+  }
+}
+</script>
+<template>
+ 
+    <h1>Hello Cả Nhà Nhế</h1>
+    <button type="button" @click="count++">count is {{ count }}</button>
+    <button type="button" @click="reset">Reset</button>
+</template>
+
+<style scoped>
+
+</style>
