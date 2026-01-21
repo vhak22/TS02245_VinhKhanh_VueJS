d5a8717da809776755d943914b28e7e3a3f43b6d
vinhkhanh <khanhnguyeen2208@gmail.com>
2026-01-22 00:19:57 +0700

    update slide 4

--- SlideDemo/Slide4/src/App.vue
+++ SlideDemo/Slide4/src/App.vue
@@ -9,7 +9,7 @@ import Trang34 from './components/Trang34.vue';
   <!-- <div>
     <a href="https://vite.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
+    </a>n
     <a href="https://vuejs.org/" target="_blank">
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
