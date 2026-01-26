940a64d751f062410509cc3a273678fada30ae3e
vinhkhanh <khanhnguyeen2208@gmail.com>
2026-01-26 21:00:47 +0700

    done bai2Lab5

---
+++ Lab/lab5/src/components/Bai2.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="p-5">
+    <input v-model="userInput" class="form-control" placeholder="Nhập dữ liệu vào đây" />
+    <p class="mt-2">Giá trị bạn nhập: {{ userInput }}</p>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+const userInput = ref('FPL'); // Giá trị mặc định là 'FPL' [cite: 3253]
+</script>
